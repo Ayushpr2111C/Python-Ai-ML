c408da1d0df6c89513ab33e290e224750925a67c
Ayush pratap <abhishekkumar39327@gmail.com>
2026-07-13 17:08:30 +0530

    starting streamlit

---
+++ calculator.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+st.title("My first app")
+st.write("Hello world")
