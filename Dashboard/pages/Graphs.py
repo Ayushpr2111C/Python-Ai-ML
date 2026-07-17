15fe92a3e814c9ff82008c25aeec7dbfc8a750d0
Ayush pratap <abhishekkumar39327@gmail.com>
2026-07-17 15:35:14 +0530

    updated graphs code

--- Dashboard/pages/Graphs.py
+++ Dashboard/pages/Graphs.py
@@ -15,17 +15,13 @@ options = st.selectbox(
     label="Select Graph Type",
     options=["Bar Chart","Line Chart","Scatter Plot"]
 )
-x_terminal = st.selectbox(
-    label="Select X-axis", 
-    options=df.columns
-)
 
 if options == "Bar Chart":
-    fig = px.bar(df, x=x_terminal, y="total", color_continuous_scale="Viridis", template="plotly_dark", title=f"Total Car Crashes by {x_terminal}")
+    fig = px.bar(df, x='State', y="total", color_continuous_scale="Viridis", template="plotly_dark", title=f"Total Car Crashes by State")
     st.plotly_chart(fig)
 elif options == "Line Chart":
-    fig = px.line(df, x=x_terminal, y="total", template="plotly_dark", title=f"Total Car Crashes by {x_terminal}")
+    fig = px.line(df, x='State', y="total", template="plotly_dark", title=f"Total Car Crashes by State")
     st.plotly_chart(fig)
 elif options == "Scatter Plot":
-    fig = px.scatter(df, x=x_terminal, y="total",color_continuous_scale="Viridis", size="total", template="plotly_dark", title=f"Total Car Crashes vs {x_terminal}")
+    fig = px.scatter(df, x='State', y="total",color_continuous_scale="Viridis", size="total", template="plotly_dark", title=f"Total Car Crashes vs State")
     st.plotly_chart(fig)
---
+++ presentation/home.py
@@ -0,0 +1,4 @@
+import streamlit as st 
+
+st.title("Welcome to my Dashboard")
+st.markdown("Performs all the operation easily")
